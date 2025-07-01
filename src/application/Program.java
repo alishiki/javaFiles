a47fb770fb87357aa647723df12a359643d9e16d
Shizuka <skimpara@gmail.com>
2025-07-01 15:56:17 -0300

    v.3 javaFiles - Files

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,12 @@
 package application;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Program {
     public static void main (String[] args){
         String pathFile = "C:\\Users\\alish\\IdeaProjects\\JavaFiles\\Presentation.txt";
+        Scanner scanner = new Scanner(System.in);
 
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(pathFile))){
             /*
@@ -20,5 +22,20 @@ public class Program {
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
         }
+
+        try (BufferedWriter bufferedWriter = new BufferedWriter (new FileWriter(pathFile, true))){
+            /*
+            No FileWriter(pathFile, true), sem o "true", toda vez que executasse o arquivo,
+            o conteúdo seria apagado e criaria um novo arquivo de texto.
+             */
+            System.out.print("\nADD A TEXT TO FILE: ");
+            String text = scanner.nextLine();
+            bufferedWriter.newLine();
+            bufferedWriter.newLine();
+            bufferedWriter.write(text);
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
+
 }
